34089343adac2d9d2227a27d79032d7aff768fb9
SumiSeo <qkobr94@gmail.com>
2021-08-30 21:43:46 +0900

    added founder page

--- src/components/Founder.js
+++ src/components/Founder.js
@@ -3,9 +3,28 @@ import React from "react";
 
 const Founder = ()=>{
     return (
-        <div>
-            hihi
+        <div class="founder__container">
+            <div className="founder__column"> </div>
+            <div className="founder__column">
+                <div className="double__quotes">
+                <i className="fas fa-quote-left"></i>
+                </div>
+                <p className="double__hello">
+                Hello, I am Sumi 😄
+                <br></br>
+                I am Full-stack developer who 
+                has proficiency including both frontend and backend work — HTML/SCSS, JavaScript, React, PHP, SQL, and third-party APIs. 
+                </p>
+            </div>
+
+            <div className="arrow">
+                <i class="fas fa-chevron-right"></i>
+            </div>
+            <div className="arrow">
+                <i class="fas fa-chevron-left"></i>
+            </div>
         </div>
+        
     )
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
                 <Link href="/" className="link logo__link">Cadre Du Monde</Link>
              </div>
              <div className="links">
+                <Link href="/" className="link about__link">Home</Link>
                 <Link href="/about" className="link about__link">About</Link>
                 <Link href="/founder" className="link founder__link">Founder</Link>
             </div>
