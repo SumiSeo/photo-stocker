3811444914f6a655c6b46581fae451bcea806dc3
SumiSeo <qkobr94@gmail.com>
2021-09-01 22:13:23 +0900

    finished slide finally

--- src/components/Founder.js
+++ src/components/Founder.js
@@ -14,11 +14,12 @@ const Founder = ()=>{
     slides.forEach((slide, i)=>{
             console.log(slide);
             console.log(i);
-            slide.style.transform = `translateX(${i * 100}%) translateY(${- i * 100}%)`;
+            slide.style.transform = `translateX(${i * 100}%)`;
+            
     });
 
     },[])
-    const [index, setSelectedIndex] = useState(0);
+
     // 
 
     const onLeftClick = () =>{
@@ -69,61 +70,25 @@ const Founder = ()=>{
                     <i className="fas fa-chevron-left"></i>
             </div>
             <div className="founder__container">
+        
+                    <article className="founder__box">
+                        <div className="founder__img1"></div>
+                    </article>
+                    <article className="founder__box">
+                        <div className="founder__img2"></div>
+                    </article>
+                    <article className="founder__box">
+                        <div className="founder__img2"></div>  
+                    </article>
+
+
+                  
+
+              
                 
-                <div className="founder__box">
-                    <div className="founder__column founder__img1"> </div>
-                    <div className="founder__column">
-                        <div className="double__quotes">
-                        <i className="fas fa-quote-left"></i>
-                        </div>
-                        <p className="double__hello">
-                        Hello, I am Sumi 😄
-                        <br></br>
-                        I am Full-stack developer who 
-                        has proficiency including both frontend and backend work — HTML/SCSS, JavaScript, React, PHP, SQL, and third-party APIs. 
-                        </p>
-                    </div>
-
-                   
-                </div>
-
-{/* //////////////////////////////////// */}
-                <div className="founder__box">
-                    <div className="founder__column founder__img2"> </div>
-                    <div className="founder__column">
-                        <div className="double__quotes">
-                        <i className="fas fa-quote-left"></i>
-                        </div>
-                        <p className="double__hello">
-                        I love creating things from scratch 🤩
-                        <br></br>
-                        I graduated Korea National University of Arts.  I made several short-films, wrote disseration about an art.
-                        All these experiences drove me to find a way to do web development.
-
-                        </p>
-                    </div>
-                </div>
-
-                {/* ///////////////////////
-                 */}
-                <div className="founder__box">
-                    <div className="founder__column founder__img3"> </div>
-                    <div className="founder__column">
-                        <div className="double__quotes">
-                        <i className="fas fa-quote-left"></i>
-                        </div>
-                        <p className="double__hello">
-                        I am a team player 🤓
-                        <br></br>
-                        Since I have the background in design and marketing, I have followed the history of web development.
-                        I enjoy to be on a team and build innovative things together.
-                        </p>
-                    </div>
-                </div>
             </div>
 
-            
-            
+           
         </div>
         
     )
