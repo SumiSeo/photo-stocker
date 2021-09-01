bc18247a6eecb24b763d9e96ccda2248f40b3406
SumiSeo <qkobr94@gmail.com>
2021-09-01 23:56:29 +0900

    deleted useeffect

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,8 @@
-import React,{useState, useEffect} from "react";
+import React,{useState} from "react";
 
 const SearchBar = ({onFormSubmit})=>{  
     const [query, setQuery] = useState("");
-    useEffect(()=>{
-        onFormSubmit("art");
-    },[]);
+    
 
     const onSubmit = (e)=>{
         e.preventDefault();
